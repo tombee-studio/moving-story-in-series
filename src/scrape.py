2593ffe410c68e1e1dae5798ca4e60415cb91639
Tomoya Bansho <tomomail1997@gmail.com>
2021-01-14 22:41:44 +0900

    suit windows

--- src/scrape.py
+++ src/scrape.py
@@ -3,8 +3,10 @@ import tqdm
 import re
 import sys
 import os
+import glob
 
 from dotenv import load_dotenv
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from static import COLUMNS
 
@@ -20,7 +22,9 @@ EXECUTABLE_PATH = os.getenv('CHROME_DRIVER_EXECUTABLE_PATH')
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--log-level=3')
 driver = webdriver.Chrome(executable_path=EXECUTABLE_PATH, options=options)
+driver.set_page_load_timeout(15)
 wait = WebDriverWait(driver, 10)
 
 
@@ -48,24 +52,30 @@ def scrape(title_id, t):
     for episode in range(1, episodes_num + 1):
         t.set_description('%s %d/%d' % (title, episode, episodes_num))
         driver.get('https://www.b-ch.com/titles/%s/%s/' % (title_id, str(episode).zfill(3)))
-        wait.until(lambda driver: text_to_be_met_with_condition(driver,
-                                                                By.CLASS_NAME,
-                                                                'bch-c-rating-plays__num',
-                                                                episode,
-                                                                re.compile('[0-9,]+')))
-        txt = driver.find_element_by_class_name('bch-c-rating-plays__num').text.replace(',', '')
-        rating_plays = int(txt)
-        story_title = driver.find_element_by_class_name('bch-p-heading-mov__summary').text
-        df = df.append(pd.DataFrame([[episode, story_title, rating_plays]], columns=COLUMNS))
-        time.sleep(2.0)
-    df.to_excel('excel/%s-%s.xlsx' % (title, title_id))
+        try:
+            wait.until(lambda driver: text_to_be_met_with_condition(driver,
+                                                                    By.CLASS_NAME,
+                                                                    'bch-c-rating-plays__num',
+                                                                    episode,
+                                                                    re.compile('[0-9,]+')))
+            txt = driver.find_element_by_class_name('bch-c-rating-plays__num').text.replace(',', '')
+            rating_plays = int(txt)
+            story_title = driver.find_element_by_class_name('bch-p-heading-mov__summary').text
+            df = df.append(pd.DataFrame([[episode, story_title, rating_plays]], columns=COLUMNS))
+        except TimeoutException as err:
+            t.set_description('can\'t load %s. continue...' % title)
+        time.sleep(5.0)
+    df.to_excel('excel/%s.xlsx' % title_id)
 
 
 def main(loadfile):
+    files = list(map(lambda file: os.path.splitext(os.path.basename(file))[0], glob.glob('excel/*.xlsx')))
     with open(loadfile) as f:
         t = tqdm.tqdm(f.readlines())
         for line in t:
-            scrape(title_id=line.strip(), t=t)
+            title_id = line.strip()
+            if title_id not in files:
+                scrape(title_id=title_id, t=t)
 
 
 if __name__ == '__main__':
